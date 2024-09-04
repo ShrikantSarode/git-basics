100eadb261c491a2dc7e04a6af9f31c0b4c33eab
[Shrikant Sarode] <shrikantsarode2001@gmail.com>
2024-09-04 09:46:46 +0530

    Feature 1 added again

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function demo(name) {
 let arr = ["a", "b", "c", "d", "e"];
 console.log(arr);
 
+let features1 = () => {
+  console.log("This is features A");
+  console.log("This is features B");
+  console.log("This is features C");
+};
+
 let features2 = () => {
   console.log("This is features D");
   console.log("This is features E");
